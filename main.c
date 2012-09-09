4cc7d03f3b15f27ec7b1bbe90d5e05d49113d6e3
fritz <fritz.groes@gmail.com>
2012-09-10 00:43:07 +0200

    basic movement support via keyboard

--- main.c
+++ main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
+#include <stdbool.h>
 //#include <stdlib.h>
 #include <GL/glut.h>
+
+
+GLfloat position[3] = {0, 0, 0};
+GLfloat speed = 0.1;
+bool keyStates[256];
 
 
 void init()
@@ -31,24 +37,57 @@ void reshape(int width, int height)
 }	
 
 
+void glTranslatev(GLfloat *v)
+{
+	glTranslatef(v[0], v[1], v[2]);
+}
+
+
 void display()
 {
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	gluLookAt(0, 0, 0, 0, 0, -100, 0, 1, 0);
 	glPushMatrix();
-	glTranslatef(0, 0, -3);
+	glTranslatev(position);
 	glBegin(GL_TRIANGLES);
 		glColor3f(0, 0, 1);
-		glVertex3f(0, 0, 0);
-		glVertex3f(0, 1, 0);
-		glVertex3f(1, 0.5, 0);
+		glVertex3f(0, 0, -3);
+		glVertex3f(0, 1, -3);
+		glVertex3f(1, 0.5, -3);
 		glColor3f(1, 0.5, 0);
-		glVertex3f(-2, -2, -5);
-		glVertex3f(2, -2, -5);
-		glVertex3f(0, 2, -5);
+		glVertex3f(-2, -2, -8);
+		glVertex3f(2, -2, -8);
+		glVertex3f(0, 2, -8);
 	glEnd();
 	glPopMatrix();
 	glutSwapBuffers();
+}	
+
+
+void keyPressed(unsigned char key, int x, int y)
+{
+	keyStates[key] = true;
+}
+
+
+void keyReleased(unsigned char key, int x, int y)
+{
+	keyStates[key] = false;
+}
+
+
+void timer(int n)
+{
+	if (keyStates['w'])
+		position[2] += speed;
+	if (keyStates['s'])
+		position[2] -= speed;
+	if (keyStates['d'])
+		position[0] -= speed;
+	if (keyStates['a'])
+		position[0] += speed;
+	glutPostRedisplay();
+	glutTimerFunc(20, timer, 0);
 }	
 
 
@@ -60,8 +99,11 @@ int main(int argc, char **argv)
 	glutInitWindowPosition(100, 100);
 	glutCreateWindow("fps");
 	init();
-	glutReshapeFunc(reshape);
 	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutKeyboardFunc(keyPressed);
+	glutKeyboardUpFunc(keyReleased);
+	glutTimerFunc(20, timer, 0);
 	glutMainLoop();
 	return 0;
 }
